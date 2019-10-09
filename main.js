11d52f40159ce7ac4bfd59ce4229763fa8ddf319
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-09 15:10:55 -0400

    two questions complete

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function ulAppender(ulNode) {
+    const findUl = document.querySelector('ul');
+    findUl.appendChild(ulNode);
+}
+
+  
+function divAppender (divNode){
+const findDiv = document.querySelector ('div');
+    findDiv.appendChild(divNode);
+}
+
+function strAdder (newStr){
+    const newLi= document.createElement('li');
+    newLi.innerText = newStr;
+} 
 
