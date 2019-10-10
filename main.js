62411ddf114115f9c43bcd4bcf8e98e4f954e631
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-10 06:11:00 -0400

    almost done with functions

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 function ulAppender(ulNode) {
-    const findUl = document.querySelector('ul');
-    findUl.appendChild(ulNode);
+    const ul = document.querySelector('ul');
+    ul.appendChild(ulNode);
 }
 
   
 function divAppender (divNode){
-const findDiv = document.querySelector ('div');
-    findDiv.appendChild(divNode);
+const div = document.querySelector ('#lorem');
+    div.appendChild(divNode);
 }
 
 function strAdder (newStr){
     const newLi= document.createElement('li');
     newLi.innerText = newStr;
+    return newLi;
 } 
 
-function imgSource(imageStr){
-    const findImg = document.querySelector('src');
-    findImg.innerText = imageStr ;
+function imgSource(image1, str1){
+    const findImg = document.querySelector('image1');
+    findImg.src = str;
 }
 
 function classEqual (element1, element2){
-    const findElement1 = document.querySelector(element1);
-    const findElement2 = document.querySelector(element2);
-    findElement1.getElementClassName    
+    const elementLoc1 = document.querySelector(element1);
+    const elementLoc2 = document.querySelector(element2);
+    elementLoc2.className = elementLoc1.className;    
 
+}
+function tagAndId(tagName, id){
+    const newElement = document.createElement(tagName);
+    newElement.id = id;
+    return newElement; 
+}
+
+function twoString(colorString, idString){
+    const foundIdString = document.querySelector(idString);
+    foundIdString.style.color = colorString;
 }
